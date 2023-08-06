f17177e616a6c5d74738c6f64fdb869d6a871826
Ashish Kumar <kumarashish97713@gmail.com>
2023-08-06 11:56:44 +0530

    full project with storybook in search component

--- src/component/search/Search.jsx
+++ src/component/search/Search.jsx
@@ -1,7 +1,7 @@
 import "./Search.css";
 
 const Search = ({ searchText, onChange, myVal, ...rest }) => {
-return <input type="text" value={searchText} onChange={onChange} className={myVal} {...rest} />
+return <input type="text" placeholder="Enter Order ID" value={searchText} onChange={onChange} className={myVal} {...rest} />
 }
 
 export default Search
--- src/component/search/Search.stories.jsx
+++ src/component/search/Search.stories.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import Search from "./Search"
 
 export default {
-    label : 'Seach Input Text',
+    label : 'Search Input Text',
     component : Search
 }
 
